a469ad7afd542f672905b52b6040b1edf0d8d2ca
PTL <tqc.dev@gmail.com>
2025-06-09 16:22:30 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, send_file   # เพิ่ม send_file
+from flask import Flask, jsonify, send_file
 from flask_cors import CORS
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
@@ -8,14 +8,16 @@ CORS(app)
 
 @app.route('/')
 def home():
-    # ส่ง index.html ที่อยู่ใน root directory
     return send_file('index.html')
 
 @app.route('/sheet-data')
 def sheet_data():
     SERVICE_ACCOUNT_FILE = '/etc/secrets/service-account.json'
     SPREADSHEET_ID = '1zNBBwHesBSFBNejaH4uTGqD90I4snSMxTcxODSQdGiE'
-    RANGE = 'Remain Par By Group Code!A1:U405'
+    RANGES = [
+        'Remain Par By Group Code!A1:U405',
+        'BlackByGroupCode!A1:U405'
+    ]
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
     creds = service_account.Credentials.from_service_account_file(
@@ -23,7 +25,7 @@ def sheet_data():
     service = build('sheets', 'v4', credentials=creds)
     result = service.spreadsheets().get(
         spreadsheetId=SPREADSHEET_ID,
-        ranges=[RANGE],
+        ranges=RANGES,
         fields='sheets.data.rowData.values.formattedValue,sheets.data.rowData.values.effectiveFormat'
     ).execute()
     return jsonify(result)
